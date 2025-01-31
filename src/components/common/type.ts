6ef8ab8a1d08f890634e54e00617e6f08dd77284
jinsol <skek298@naver.com>
2025-01-31 14:34:41 +0900

    💡refactor: 파일 위치 수정

--- src/components/common/type.ts
+++ src/components/common/type.ts
@@ -1,3 +1,41 @@
 export type ErrorScreenProps = {
-  errorMessage: string;
+    errorMessage: string;
+};
+
+export type ProblemContentProps = {
+    title?: string;
+    description?: string;
+};
+
+export type HintButtonProps = {
+    hint?: string;
+};
+
+export type SectorViewProps = {
+    setSelectedSector: (sector: string) => void;
+    handleOnPress: () => void;
+};
+
+export type ProblemItemProps = {
+    problem: {
+        problem_id: number;
+        title: string;
+    }[];
+    handleEndReached: () => void;
+    isFetchingNextPage: boolean;
+};
+
+export type ProblemListContentProps = {
+    isLoading: boolean;
+    isError: boolean;
+    problems: { problem_id: number; title: string }[];
+    handleEndReached: () => void;
+    isFetchingNextPage: boolean;
+    fetchNextPage: () => void;
+    hasNextPage: boolean;
+};
+
+export type HintViewProps = {
+    setClickHint: (clickHint: boolean) => void;
+    hint?: string;
 };

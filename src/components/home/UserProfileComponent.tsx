0cb430e9005f037929938e03bff88c0c7ed33fe8
jinsol <skek298@naver.com>
2025-02-11 00:18:17 +0900

    🔥HOT_FIX: 폴더 위치 변경에 따른 import 수정

--- src/components/home/UserProfileComponent.tsx
+++ src/components/home/UserProfileComponent.tsx
@@ -3,31 +3,21 @@ import { Image, Text, TouchableOpacity } from 'react-native';
 import { UserProfileProps } from './type';
 import { useNavigation } from '@react-navigation/native';
 import { BottomTabNavigationProp } from '@react-navigation/bottom-tabs';
-import { RootTabParamList } from '../../../type';
+import { RootTabParamList } from '../navigation/type';
 
-const UserProfileComponent = ({
-  profileImage,
-  nickName,
-  userName,
-}: UserProfileProps) => {
-  const navigation = useNavigation<BottomTabNavigationProp<RootTabParamList>>();
+const UserProfileComponent = ({ profileImage, nickName, userName }: UserProfileProps) => {
+    const navigation = useNavigation<BottomTabNavigationProp<RootTabParamList>>();
 
-  const handleDashBoard = () => {
-    navigation.navigate('DashBoardScreen');
-  };
+    const handleDashBoard = () => {
+        navigation.navigate('DashBoardScreen');
+    };
 
-  return (
-    <TouchableOpacity
-      className="w-1/2 h-full flex justify-center items-center"
-      onPress={handleDashBoard}
-    >
-      <Image
-        source={{ uri: profileImage }}
-        className="w-20 h-20 rounded-[50%]"
-      />
-      <Text className="py-2">{nickName}</Text>
-      <Text>{userName}</Text>
-    </TouchableOpacity>
-  );
+    return (
+        <TouchableOpacity className="w-1/2 h-full flex justify-center items-center" onPress={handleDashBoard}>
+            <Image source={{ uri: profileImage }} className="w-20 h-20 rounded-[50%]" />
+            <Text className="py-2">{nickName}</Text>
+            <Text>{userName}</Text>
+        </TouchableOpacity>
+    );
 };
 export default UserProfileComponent;
